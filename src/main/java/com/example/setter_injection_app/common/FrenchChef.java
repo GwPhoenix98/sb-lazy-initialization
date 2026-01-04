42a81052b17b78192cf7766fc2081e8042d558c2
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-04 12:34:12 +0200

    Implement constructor for all chef

--- src/main/java/com/example/setter_injection_app/common/FrenchChef.java
+++ src/main/java/com/example/setter_injection_app/common/FrenchChef.java
@@ -1,11 +1,14 @@
 package com.example.setter_injection_app.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class FrenchChef implements Chef {
+
+    public FrenchChef() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyRecipe() {
         return "Prepara o reteta franceza";
--- src/main/java/com/example/setter_injection_app/common/TurkishChef.java
+++ src/main/java/com/example/setter_injection_app/common/TurkishChef.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TurkishChef implements Chef {
+
+    public TurkishChef() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyRecipe() {
         return "Prepara bucate turcesti!";
